b09ac620057c0bf44d8669b246c2fd5dc6c12b6b
Carlos <carlos@storylabs.dev>
2022-09-21 14:49:34 -0600

    17

--- deleteme/main.tf
+++ deleteme/main.tf
@@ -1,23 +1,44 @@
-locals {
-  filename = "test.txt"
+variable "name" {
+  type        = string
+  description = "Nombre de la instancia"
+
+  validation {
+    condition     = length(var.name) > 5
+    error_message = "El nombre de la instancias debe ser mayor a 5 caracteres."
+  }
+}
+variable "size" {
+  type        = string
+  description = "Tamaño de la instancia: small - large"
+
+  validation {
+    condition     = var.size == "nano" || var.size == "micro"
+    error_message = "Las instancias permitidas son: nano || micro."
+  }
+}
+variable "nano" {
+  type    = string
+  default = "t3a.nano"
+}
+variable "micro" {
+  type    = string
+  default = "t3a.micro"
 }
 
-module "file-module" {
-  source = "./file-module"
-  filename = local.filename
 
+resource "aws_instance" "server" {
+  ami           = "ami-033b95fb8079dc481"
+  instance_type = var.size == "nano" ? var.nano : var.micro
+  tags = {
+    Name = var.name
+  }
+}
+output "instance_size" {
+  value = aws_instance.server.instance_type
 }
 
-module "file-module-dos" {
-  source = "./file-module"
 
-  filename = "dos.txt"
-}
 
-output "archivo" {
-  value       = module.file-module.filename
-}
-
-output "archivo-dos" {
-  value       = module.file-module-dos.filename
+provider "aws" {
+  region = "us-east-1"
 }
