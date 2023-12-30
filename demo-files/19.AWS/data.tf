8a34dfd73a3206c9187dfd832814db5608b13af8
Carlos Chacon <carlos@storylabs.dev>
2023-12-29 21:19:28 -0600

    aws ec2 describe-images

--- demo-files/19.AWS/data.tf
+++ demo-files/19.AWS/data.tf
@@ -13,3 +13,6 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+
+# aws ec2 describe-images --owners 099720109477 --filters "Name=name,Values=*ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*" --query 'Images|sort_by(@, &CreationDate)|[-1].[ImageId, Name]'
+# aws ec2 describe-images --owners 099720109477 --filters "Name=name,Values=*ubuntu/images/hvm-ssd/ubuntu-jammy-22.04*" --query 'Images|sort_by(@, &CreationDate)|[-1].[ImageId, Name]'
