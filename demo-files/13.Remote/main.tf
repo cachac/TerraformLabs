a82f5b88e3f86b80d94a15500c8147006ca7e977
Carlos Chacon <carlos@storylabs.dev>
2024-04-22 23:11:18 -0600

    13 remote

--- demo-files/13.Remote/main.tf
+++ demo-files/13.Remote/main.tf
@@ -3,13 +3,6 @@ resource "local_file" "state_demo" {
   content  = "Archivo de estado: local"
 }
 
-resource "aws_s3_bucket_ownership_controls" "state_control" {
-  bucket = aws_s3_bucket.remote_state.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
 resource "aws_s3_bucket" "remote_state" {
   bucket = "terralabs-remote-state-01" # el nombre del bucket debe ser unico.
 
@@ -18,10 +11,30 @@ resource "aws_s3_bucket" "remote_state" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "access" {
+  bucket = aws_s3_bucket.remote_state.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
+resource "aws_s3_bucket_ownership_controls" "owner" {
+  depends_on = [aws_s3_bucket_public_access_block.access]
+
+  bucket = aws_s3_bucket.remote_state.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "remote_state" {
-  depends_on = [aws_s3_bucket_ownership_controls.state_control]
+	depends_on = [aws_s3_bucket_ownership_controls.owner]
 
   bucket = aws_s3_bucket.remote_state.id
   acl    = "private"
 }
+
 
